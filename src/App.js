e231d6f40a42678a3bbc2a285cc663629e1c20ac
Hassan <noueilaty@gmail.com>
2018-01-22 14:03:39 -0600

    created component files and deleted default content

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Header } from './components/Header';
+import { MainContent } from './components/MainContent';
+import { Subheader } from './components/Subheader';
 
-class App extends Component {
+export class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Header />
+        <MainContent />
+        <Subheader />
       </div>
     );
   }
 }
-
-export default App;
